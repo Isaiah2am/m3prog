752deaf90d3c22cf8a70af8260047229a6033549
Isaiah2am <isaiahkoroma@hotmail.com>
2025-02-13 12:31:06 +0100

    commit 01 05

---
+++ M3Prog_project/public/01/status.php
@@ -0,0 +1,26 @@
+<?php
+$status = "offline";
+echo $status;
+$status = "avalible";
+echo $status;
+$status = "busy";
+echo $status;
+$status = "do not disturb";
+echo $status;
+$status = "offline";
+echo $status;
+
+$prijs = "€5";
+echo $prijs;
+$prijs = "€6";
+echo $prijs;
+$prijs = "€7";
+echo $prijs;
+$prijs = "€8";
+echo $prijs;
+$prijs = "€9";
+echo $prijs;
+$prijs = "€10";
+
+?>
+
