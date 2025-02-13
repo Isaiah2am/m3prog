7a17cab26f1e33f0ef6abc19fb3c0a2c90cae813
Isaiah2am <isaiahkoroma@hotmail.com>
2025-02-13 12:18:08 +0100

    commit 01 04

--- M3Prog_project/public/01/phphtml_var.php
+++ M3Prog_project/public/01/phphtml_var.php
@@ -3,24 +3,29 @@ $title = "Mijn mooie website";
 $datum = date('d-m-y');
 $tijd = date("H:i");
 $favoritegame= "super metroid";
-$mooistegetal= 42;
+$mooistegetal= 42;  
+$favoriteshow = "spongebob";
+$leukestad = "amsterdam";
 
-echo $title;
-echo $datum;
-echo $tijd;
-echo $favoritegame;
-echo $mooistegetal;
 
 ?>
 
 <!doctype html>
 <html lang="en">
     <head>
-        <title><?php echo "Titel van de pagina" ?></title>
+        <title><?php echo $title ?></title>
     </head>
     <body>
-        <h2>De datum vandaag is: <?php echo date('d-m-Y')?>, en de tijd is <?php echo date("H:i")?> uur.
+
+        <h2>De datum vandaag is: <?php echo $datum?>, en de tijd is <?php echo $title?> uur.
         
-        <p><h2>Mijn favoriete game</h2><?php echo "Super metroid";?></p>
+        <p><h2>Mijn favoriete game</h2><?php echo $favoritegame?></p>
+
+        <p><h2>mooistegetal:</h2><?= $mooistegetal?> </p>
+
+        <p><h2>favorite show</h2><?=$favoriteshow?> </P>
+
+        <p><h2>favorite stad</h2><?=$leukestad?></p>
+        
     </body>
 </html>
