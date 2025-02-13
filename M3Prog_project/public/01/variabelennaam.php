257504ee75fc9ebd94d6f421c57351cf531bf723
Isaiah2am <isaiahkoroma@hotmail.com>
2025-02-13 12:38:47 +0100

    comit 01 06

---
+++ M3Prog_project/public/01/variabelennaam.php
@@ -0,0 +1,24 @@
+<?php
+$auto2 = "yo"	;
+$_naam	= "yo"	;
+$auto = "yo"	;
+//$auto/;
+//$huis&tuin	;
+$naw	= "yo"	;
+//$22	;
+//$$auto;	
+$kleurVanAuto= "yo"		;
+//$software-dev;
+
+
+echo $auto2;
+echo $_naam;
+echo $auto;
+//echo $auto/;
+//  echo $huis&tuin;
+//echo $22;
+//echo $$auto;
+echo $kleurVanAuto;
+//echo $software-dev;
+
+?>
