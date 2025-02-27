df2ca52868edb2c3668783ecf0f444a81f94c145
Isaiah2am <isaiahkoroma@hotmail.com>
2025-02-27 12:39:30 +0100

    lats opdra bom shaka laka fishish
    leest iemand dit ooit

---
+++ M3Prog_project/public/02/item.php
@@ -0,0 +1,25 @@
+<?php
+
+$item_name = "Ghostflame" ;
+$stat1 = "+190-240% Enhanced Damage";
+$stat2 = "+108 Magic Damage" ;
+$stat3 = "+2 To Light Radius" ;
+$stat4 = "10-15% Mana Stolen Per Hit ";
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?= $item_name; ?></h1>
+    <p><?= $stat1; ?></p>
+    <p><?= $stat2; ?></p>
+    <p><?= $stat3; ?></p>
+    <p><?= $stat4; ?></p>
+</body>
+</html>
