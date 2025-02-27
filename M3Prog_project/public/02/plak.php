7eac89595c0e2df010f42044394a48d910966b0c
Isaiah2am <isaiahkoroma@hotmail.com>
2025-02-27 12:03:01 +0100

    comit 202

---
+++ M3Prog_project/public/02/plak.php
@@ -0,0 +1,16 @@
+<?php
+
+$standartstring= 'hello world<br>';
+$extrastring = "extraworld<br>";
+
+$voornaam = "luigi<br>";
+$achternaam = "mario<br>";
+
+
+$fullname = $voornaam . " " . $achternaam;
+
+
+echo $fullname;
+echo $standartstring;
+echo $extrastring;
+?>
