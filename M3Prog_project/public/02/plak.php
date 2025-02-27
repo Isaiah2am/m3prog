93722b467f6bc642b38dd01a584c5bb242fff57c
Isaiah2am <isaiahkoroma@hotmail.com>
2025-02-27 12:10:56 +0100

    plak2.0

--- M3Prog_project/public/02/plak.php
+++ M3Prog_project/public/02/plak.php
@@ -7,10 +7,31 @@ $voornaam = "luigi<br>";
 $achternaam = "mario<br>";
 
 
+$naam = "Yoshi";
+$ondertekentDoor = "directeur bowser";
+
+$emailtext =
+"Beste $naam<br>
+Helaas moeten wij uw verzoek afwijzen.<br>
+Onze Goomba's vinden het niet leuk om opgegeten te worden<br>
+<br>
+Hoogachtend<br>
+$ondertekentDoor
+<br>
+PS:<br>
+De princess is in haar kasteel... voor nu :>
+";
+
+
 $fullname = $voornaam . " " . $achternaam;
 
+echo $emailtext;
 
 echo $fullname;
 echo $standartstring;
 echo $extrastring;
+
+echo $naam;
+echo $ondertekentDoor;
+
 ?>
