aa3520df7ec2ddda2d9e3caf4bbf31ac6a5a4ebf
Isaiah2am <isaiahkoroma@hotmail.com>
2025-03-04 15:17:56 +0100

    yes

--- M3Prog_project/public/02/menu.php
+++ M3Prog_project/public/02/menu.php
@@ -1,5 +1,5 @@
 <?php
-// Het format voor het gerecht
+
 $gerechtFormat = "Gerecht type: <TYPEGERECHT><br><br>
 <NAAMGERECHT> €<PRIJS><br><br>
 <BESCHRIJVING><br>
@@ -8,15 +8,14 @@ Ingegredientenlijst:<br>
 Bent u allergisch? bekijk dan hieronder de allergenen:<br>
 <ALLERGENENLIJST><br>";
 
-// Variabelen voor het gerecht
-$typeGerecht = "hoofd";  // Kan 'voor', 'hoofd' of 'na' zijn
+$typeGerecht = "hoofd";  
 $naamGerecht = "Pizza Margherita";
-$prijs = 12.50;  // De prijs van het gerecht in euro's
+$prijs = 12.50;  
 $beschrijving = "Een klassieke pizza met tomatensaus, mozzarella en verse basilicum.";
 $ingredientenLijst = "Tomaat, mozzarella, basilicum, olijfolie";
 $allergenenLijst = "Gluten, melk";
+ 
 
-// Vervang de placeholders met de variabelen
 $gerechtDetails = str_replace("<TYPEGERECHT>", $typeGerecht, $gerechtFormat);
 $gerechtDetails = str_replace("<NAAMGERECHT>", $naamGerecht, $gerechtDetails);
 $gerechtDetails = str_replace("<PRIJS>", $prijs, $gerechtDetails);
@@ -24,6 +23,6 @@ $gerechtDetails = str_replace("<BESCHRIJVING>", $beschrijving, $gerechtDetails);
 $gerechtDetails = str_replace("<INGREDIENTENLIJST>", $ingredientenLijst, $gerechtDetails);
 $gerechtDetails = str_replace("<ALLERGENENLIJST>", $allergenenLijst, $gerechtDetails);
 
-// Toon het resultaat
+
 echo $gerechtDetails;
 ?>
