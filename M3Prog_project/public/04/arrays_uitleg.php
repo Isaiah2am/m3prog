3b7b107a42d8bbe1c0e17817cfeaa5d24633e5c0
Isaiah2am <isaiahkoroma@hotmail.com>
2025-03-06 11:54:07 +0100

    Update arrays_uitleg.php

--- M3Prog_project/public/04/arrays_uitleg.php
+++ M3Prog_project/public/04/arrays_uitleg.php
@@ -3,7 +3,29 @@
 $nummers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 
+echo $nummers[9];  
+echo "<br>";
+echo $nummers[1]; 
+echo "<br>";
+echo $nummers[0]; 
+echo "<br>";
+echo $nummers[5]; 
+echo"<br>";
+
+
 print_r($nummers);
+
+$dagen = ["maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag"];
+
+echo"<br>";
+echo $dagen[4]; 
+echo"<br>";
+echo $dagen[2]; 
+echo"<br>";
+echo $dagen[6]; 
+echo"<br>";
+echo $dagen[1]; 
+echo"<br>";
 
 
 ?>
