00e3642b1126f4f0c996efb362382e2ac1b0fbe6
Isaiah2am <isaiahkoroma@hotmail.com>
2025-03-06 12:02:54 +0100

    yayay

---
+++ M3Prog_project/public/04/arrays_eigen.php
@@ -0,0 +1,19 @@
+<?php
+
+$games= ["fortnite","minecraft","cod","mario","zelda"];
+
+
+
+echo"<br>";
+echo $games[2]; 
+echo"<br>";
+echo $games[4]; 
+echo"<br>";
+
+
+var_dump($games);
+echo"<br>";
+print_r($games);
+
+?>
+
