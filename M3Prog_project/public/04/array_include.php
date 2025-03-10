0ab359d42e25057bbc5d7a978c08f9e1114e4351
Isaiah2am <isaiahkoroma@hotmail.com>
2025-03-10 12:57:10 +0100

    ?

--- M3Prog_project/public/04/array_include.php
+++ M3Prog_project/public/04/array_include.php
@@ -6,6 +6,15 @@ $announcements = [
     "d.html",
     "e.html"
 ];
+
+
+$reviews = 
+[
+    ["demon's souls",10],
+    ["mario wonder",9.5],
+    ["mega man 2",8],
+]
+;
 ?>
 
 <!DOCTYPE html>
@@ -20,6 +29,21 @@ $announcements = [
     foreach ($announcements as $announcement) {
         include "announcements/$announcement";
     }
+
+    foreach ($reviews as $review) {
+
+        $title = $review[0];
+        $cijfer = $review[1];
+
+
+
+    }
+
+
     ?>
+
+<h1> <?= $title?></h1>
+<h2><?=$cijfer?></h2>
+
 </body>
 </html>
