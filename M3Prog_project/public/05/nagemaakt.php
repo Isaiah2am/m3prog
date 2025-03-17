679dd6a3eaf338b37c8bd630173ed8c73ec672c6
Isaiah2am <isaiahkoroma@hotmail.com>
2025-03-17 12:02:10 +0100

    gg

---
+++ M3Prog_project/public/05/nagemaakt.php
@@ -0,0 +1,22 @@
+<?php
+
+$dezeGaatMee=("deze waarde wordt meegenomen");
+
+function mijnprint($printtext)
+{
+    echo $printtext;
+    echo "<br>"; 
+}
+
+
+mijnprint("regel1");
+mijnprint("regel2");
+mijnprint("regel3");
+
+
+echo "dit is mijn print";
+echo "<br>";
+
+mijnprint("deze waarde wordt meegenomen");
+
+?>
