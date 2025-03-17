d56881d8a26e887e47c52ba27a220ee28d10b7c5
Isaiah2am <isaiahkoroma@hotmail.com>
2025-03-17 13:07:04 +0100

    yes

---
+++ M3Prog_project/public/05/macht2.php
@@ -0,0 +1,10 @@
+<?php
+
+$calc = macht2(9);
+
+function macht2($getal1){
+    $som = $getal1 * $getal1;
+    return $som;
+}   
+
+echo $calc;
--- M3Prog_project/public/05/returncalc.php
+++ M3Prog_project/public/05/returncalc.php
@@ -3,6 +3,7 @@
 $anser1 = add(5, 5);
 $anser2 = divide(10, 10);
 $anser3 = subtract(15, 15);
+$anser4 = multiply(15, 15);
 
 function add($getal1, $getal2){
     $som = $getal1 + $getal2;
@@ -19,12 +20,19 @@ function subtract($getal1, $getal2){
     return $som;
 }   
 
+function multiply($getal1, $getal2){
+    $som = $getal1 * $getal2;
+    return $som;
+}   
+
     
 echo $anser1;
 echo"<br>";
 echo $anser2;
 echo"<br>";
 echo $anser3;
+echo"<br>";
+echo $anser4;
 
 
 
