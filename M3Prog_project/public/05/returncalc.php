800a830a1c54ba28ac5386b7b92a31706d313ffc
Isaiah2am <isaiahkoroma@hotmail.com>
2025-03-17 12:54:42 +0100

    return calc

---
+++ M3Prog_project/public/05/returncalc.php
@@ -0,0 +1,33 @@
+<?php
+
+$anser1 = add(5, 5);
+$anser2 = divide(10, 10);
+$anser3 = subtract(15, 15);
+
+function add($getal1, $getal2){
+    $som = $getal1 + $getal2;
+    return $som;
+}   
+
+function divide($getal1, $getal2){
+    $som = $getal1 / $getal2;
+    return $som;
+}   
+
+function subtract($getal1, $getal2){
+    $som = $getal1 - $getal2;
+    return $som;
+}   
+
+    
+echo $anser1;
+echo"<br>";
+echo $anser2;
+echo"<br>";
+echo $anser3;
+
+
+
+
+
+?>
