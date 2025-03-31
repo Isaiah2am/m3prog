9d067754e6728cd248b6a2b2ff7ee71b14c0203d
Isaiah2am <isaiahkoroma@hotmail.com>
2025-03-31 12:20:58 +0200

    lesson 1 van 7

---
+++ M3Prog_project/public/07/assoc_intro.php
@@ -0,0 +1,20 @@
+<?php
+
+$tropiespergame = [
+    "A" => 11,
+    "B" => 0,
+    "C" => 9,
+    "D" => "tetris",
+
+];
+     
+
+$tropiesA = $tropiespergame["A"];
+$tropiesB = $tropiespergame["B"];
+$tropiesC = $tropiespergame["C"];
+$tropiesD = $tropiespergame["D"];
+
+echo "Tropies A: $tropiesA<br>";
+echo "Tropies B: $tropiesB<br>";
+echo "Tropies C: $tropiesC<br>";
+echo "Tropies D: $tropiesD<br>";
